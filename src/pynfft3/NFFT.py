18430cbaf533a0dfc1a270199650eb7e90726e00
Michael Nolander <113628428+mnolander@users.noreply.github.com>
2024-07-09 13:45:25 +0200

    Added init and property functions

--- src/pynfft3/NFFT.py
+++ src/pynfft3/NFFT.py
@@ -1,5 +1,7 @@
 #TODO: Text formatting
+#TODO: Combine constructors
 
+import warnings
 import ctypes
 import numpy as np
 from .flags import *
@@ -153,3 +155,150 @@ def nfft_finalize_plan(P):
 
 def finalize_plan(P):
     return nfft_finalize_plan(P)
+
+# # allocate plan memory and init with D,N,M,n,m,f1,f2
+# """
+#     nfft_init(P)
+
+# intialises the NFFT plan in C. This function does not have to be called by the user.
+
+# # Input
+# * `P` - a NFFT plan structure.
+
+# # See also
+# [`NFFT{D}`](@ref), [`nfft_finalize_plan`](@ref)
+
+def nfft_init(P):
+    D = len(P.N)
+
+    # Convert N and n to numpy arrays for passing them to C
+    Nv = np.array(P.N, dtype=np.int32)
+    n = np.array(P.n, dtype=np.int32)
+
+    # Call init for memory allocation
+    ptr = nfftlib.jnfft_alloc()
+
+    # Set the pointer
+    P.plan = ctypes.cast(ptr, ctypes.POINTER(nfft_plan))
+
+    # Initialize values
+    nfftlib.jnfft_init(
+        P.plan,
+        ctypes.c_int32(D),
+        Nv.ctypes.data_as(ctypes.POINTER(ctypes.c_int32)),
+        ctypes.c_int32(P.M),
+        n.ctypes.data_as(ctypes.POINTER(ctypes.c_int32)),
+        ctypes.c_int32(P.m),
+        ctypes.c_uint32(P.f1),
+        ctypes.c_uint32(P.f2)
+    )
+    P.init_done = True
+
+    #TODO: Test garbage collector
+    import atexit
+    atexit.register(nfft_finalize_plan, P)
+
+def init(P):
+    return nfft_init(P)
+
+# Overwrite dot notation for plan struct in order to use C memory
+def setproperty(P, v, val):
+    # Init plan if not done [usually with setting nodes]
+    if not P.init_done:
+        nfft_init(P)
+
+    # Prevent bad stuff from happening
+    if P.finalized:
+        raise ValueError("NFFT already finalized")
+
+    # Setting nodes, verification of correct size dxM
+    if v == 'x':
+        D = len(P.N)
+        if D == 1:
+            if not isinstance(val, np.ndarray) or val.dtype != np.float64:
+                raise ValueError("x has to be a Float64 vector")
+            if val.shape[0] != P.M:
+                raise ValueError("x has to be a Float64 vector of length M")
+        else:
+            if not isinstance(val, np.ndarray) or val.dtype != np.float64:
+                raise ValueError("x has to be a Float64 matrix")
+            if val.shape[0] != D or val.shape[1] != P.M:
+                raise ValueError("x has to be a Float64 matrix of size dxM")
+        
+        ptr = nfftlib.jnfft_set_x(P.plan, val.ctypes.data_as(ctypes.POINTER(ctypes.c_double)))
+        P.x = ptr
+
+    elif v == 'f':
+        if not isinstance(val, np.ndarray) or not np.issubdtype(val.dtype, np.number):
+            raise ValueError("f has to be a vector of numbers")
+        if val.shape[0] != P.M:
+            raise ValueError("f has to be a ComplexFloat64 vector of size M")
+        
+        f_complex = val.astype(np.complex128)
+        ptr = nfftlib.jnfft_set_f(P.plan, f_complex.ctypes.data_as(ctypes.POINTER(ctypes.c_complex)))
+        P.f = ptr
+        # Setting Fourier coefficients
+
+    elif v == 'fhat':
+        if not isinstance(val, np.ndarray) or not np.issubdtype(val.dtype, np.number):
+            raise ValueError("fhat has to be a vector of numbers")
+        l = np.prod(P.N)
+        if val.shape[0] != l:
+            raise ValueError("fhat has to be a ComplexFloat64 vector of size prod(N)")
+        
+        fhat_complex = val.astype(np.complex128)
+        ptr = nfftlib.jnfft_set_fhat(P.plan, fhat_complex.ctypes.data_as(ctypes.POINTER(ctypes.c_complex)))
+        P.fhat = ptr
+
+    elif v in ['plan', 'num_threads', 'init_done', 'N', 'M', 'n', 'm', 'f1', 'f2']:
+        warnings.warn(f"You can't modify {v}, please create an additional plan.")
+
+    match v:
+        case 'plan':
+            warnings.warn("You can't modify the C pointer to the NFFT plan.")
+        case 'num_threads':
+            warnings.warn("You can't currently modify the number of threads.")
+        case 'init_done':
+            warnings.warn("You can't modify this flag.")
+        case 'N':
+            warnings.warn("You can't modify the bandwidth, please create an additional plan.")
+        case 'M':
+            warnings.warn("You can't modify the number of nodes, please create an additional plan.")
+        case 'n':
+            warnings.warn("You can't modify the oversampling parameter, please create an additional plan.")
+        case 'm':
+            warnings.warn("You can't modify the window size, please create an additional plan.")
+        case 'f1':
+            warnings.warn("You can't modify the NFFT flags, please create an additional plan.")
+        case 'f2':
+            warnings.warn("You can't modify the FFTW flags, please create an additional plan.")
+        case _:
+            setattr(P, v, val)
+
+def setproperty(P, v):
+    if v == 'x':
+        if P.x is None:
+            raise AttributeError("x is not set.")
+        ptr = P.x
+        if P.D == 1:
+            return np.ctypeslib.as_array(ptr, shape=(P.M,)) # Get nodes from C memory and convert to Python type
+        else:
+            return np.ctypeslib.as_array(ptr, shape=(P.D, P.M)) # Get nodes from C memory and convert to Python type
+    
+    elif v == 'num_threads':
+        return nfftlib.nfft_get_num_threads()
+    
+    elif v == 'f':
+        if P.f is None:
+            raise AttributeError("f is not set.")
+        ptr = P.f
+        return np.ctypeslib.as_array(ptr, shape=(P.M,)) # Get function values from C memory and convert to Python type
+    
+    elif v == 'fhat':
+        if P.fhat is None:
+            raise AttributeError("fhat is not set.")
+        ptr = P.fhat
+        return np.ctypeslib.as_array(ptr, shape=(np.prod(P.N),)) # Get function values from C memory and convert to Python type
+    
+    else:
+        return P.__dict__[v]
